f3a14b2d26ef638da692de202d61c50bb6db13f6
Tiago Rodrigues de Souza <tiago.souza@basis.com.br>
2024-10-20 20:57:50 -0300

    Projeto finalizado!

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# jogo-detona-ralph
+# Detona Ralph - Jogo de Acertar o Ralph
+
+Este é um projeto de um jogo simples baseado no filme **Detona Ralph**. O objetivo do jogo é acertar o Ralph nas janelas em que ele aparece até o tempo ou suas vidas acabarem. O projeto do jogo foi criado utilizando apenas **HTML**, **CSS** e **JavaScript**. Durante o desenvolvimento, foi explorado eventos JavaScript e a manipulação de áudios para criar uma experiência de jogo divertida e interativa.
+
+## Funcionalidades
+
+- **Jogo interativo**: O Ralph aparece aleatoriamente em diferentes janelas, e o jogador precisa clicar na janela correta para ganhar pontos.
+- **Eventos JavaScript**: Utilizamos eventos de clique e temporizadores para controlar a aparição do Ralph e a interação do jogador.
+- **Manipulação de áudio**: Efeitos sonoros são tocados sempre que o Ralph é acertado ou quando o jogador erra.
+- **Pontuação dinâmica**: A cada acerto, a pontuação do jogador aumenta, e o objetivo é conseguir o máximo de pontos dentro do tempo limite.
+
+## Tecnologias Utilizadas
+
+- **HTML**: Estrutura básica do jogo e das janelas onde o Ralph aparece.
+- **CSS**: Estilização das janelas, do Ralph e da interface do jogo.
+- **JavaScript**: Lógica do jogo, controle de eventos, temporizadores e manipulação de áudios.
+
+## Como Jogar
+
+   
+Experiemente: [Clique para Jogar](https://rsouza13.github.io/jogo-detona-ralph/).
---
+++ src/scripts/engine.js
@@ -0,0 +1,95 @@
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+        lives: document.querySelector("#lives")
+    },
+    values: {
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        currentTime: 60,
+        lives:5,
+        gameOver: false,
+        soundInit: new Audio(`./src/audios/audio-game.mp3`)
+    },
+    actions: {
+        timerId: setInterval(randomSquareEnemy, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
+    }
+};
+
+function playSound(audioName){
+    let audio = new Audio(`./src/audios/${audioName}.mp3`)
+    audio.volume = 0.2;
+    audio.play();
+}
+
+function startSoundInit(){
+    state.values.soundInit.volume=0.1;
+    state.values.soundInit.play();
+}
+
+function countDown(){
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if(state.values.currentTime <= 0)
+        gameOver();
+}
+
+function gameOver(){
+    if(!state.values.gameOver){
+        clearInterval(state.actions.timerId);
+        clearInterval(state.actions.countDownTimerId);
+        state.values.lives=0;
+        state.values.soundInit.pause();
+        playSound('game-over');
+        alert('Game Over! O seu resultado foi: ' + state.values.result);
+        state.values.gameOver=true;
+    }
+}
+
+function randomSquareEnemy(){
+    state.view.squares.forEach((square)=>{
+        square.classList.remove("enemy");
+    });
+
+    let randomNumberEnemy = Math.floor(Math.random() * 9);
+    let randomSquareEnemy = state.view.squares[randomNumberEnemy];
+
+    randomSquareEnemy.classList.add("enemy");
+    state.values.hitPosition = randomSquareEnemy.id;
+}
+
+
+function addListenerHitBox(){
+    state.view.squares.forEach((square)=>{
+        square.addEventListener("mousedown", ()=>{
+            if(square.id === state.values.hitPosition && state.values.lives > 0 && state.values.currentTime > 0){
+                playSound('hit');
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+            }else{
+                playSound('err');
+                if(state.values.lives <= 0){
+                    gameOver();
+                }else{
+                    state.values.lives--;
+                    state.view.lives.textContent = state.values.lives;
+                }
+            }
+        });
+    });
+}
+
+function init() {
+    startSoundInit();
+    state.view.lives.textContent = state.values.lives;
+    addListenerHitBox();
+}
+
+init();
